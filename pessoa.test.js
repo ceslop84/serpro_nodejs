3c6dc091762c70fa057d8202c894407d4dfda913
Cesar Rafael Lopes <ceslop84@gmail.com>
2023-08-26 12:13:10 -0300

    Ajuste

--- pessoa.test.js
+++ pessoa.test.js
@@ -4,6 +4,6 @@ test('Função para ser testada', () => {
     const p = new Pessoa("masculino");
     const observado = p.funcaoParaSerTestada(6,3);
     let a = 6, b=3;
-    real = a+b;
+    let real = a+b;
     expect(observado).toEqual(real);
 })
